10ea9348fdef909dd54ce3dc0d0ab732b571c66a
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-14 12:50:14 -0600

    Build login component

---
+++ src/Login/Login.js
@@ -0,0 +1,64 @@
+import './Login.css'
+import { useState } from 'react'
+import userData from '../sampleData'
+
+const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const updateUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  // const login = (id) => {
+  //   userData.find(user => user.id === )
+  // }
+
+  // const validate = () => {
+
+  // }
+
+  return (
+    <form className='login-form' onSubmit='tbd'>
+      <input
+        type='text'
+        id='usernameInput'
+        name='username'
+        value={username}
+        className='username-input no-outline'
+        placeholder='username:'
+        aria-label='username input'
+        onChange={updateUsername}
+      />
+      <br />
+      <br />
+      <input
+        type='password'
+        id='passwordInput'
+        name='password'
+        value={password}
+        className='password-input no-outline'
+        placeholder='password:'
+        aria-label='password input'
+        onChange={updatePassword}
+      />
+      <br />
+      <br />
+      <div className='button-container'>
+        <input
+          type='submit'
+          id='submit'
+          value='login'
+          className='login-button'
+          onClick='tbd'
+        />
+      </div>
+    </form>
+  )
+}
+
+export default Login
