eab617a845ff622cbeb46b8d3c838873148b1cec
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-14 13:58:31 -0600

    Built dashboard component

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,18 +1,27 @@
 import './Dashboard.css'
+import profPic from '../images/lifter-pic.jpg'
 
 const Dashboard = () => {
   return (
     <section className='dashboard'>
-      <header>
-        <h1>Welcome, User!</h1>
-        {/* <img src='tbd' className='profile-pic' alt='user-prof-pic' /> */}
-      </header>
-      <section className='button-container'>
-        <button className='social-button'>Social</button>
-        <button className='create-workout-button'>Create Workout</button>
-        <button className='suggested-button'>Suggested Workouts</button>
-        <button className='past-workout-button'>Past Workouts</button>
-      </section>
+      <div className='welcome-buttons-container'>
+        <h1 className='welcome-message'>Welcome, User!</h1>
+        <section className='button-container'>
+          <button className='social-button dash-button'>Social</button>
+          <button className='create-workout-button dash-button'>
+            Create Workout
+          </button>
+          <button className='suggested-button dash-button'>
+            Suggested Workouts
+          </button>
+          <button className='past-workout-button dash-button'>
+            Past Workouts
+          </button>
+        </section>
+      </div>
+      <div className='img-container'>
+        <img src={profPic} className='profile-pic' alt='user-prof-pic' />
+      </div>
     </section>
   )
 }
