a86de8a7e6626012aefaa0a3eacf38fbc8645848
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-14 12:48:41 -0600

    Add sample user data

---
+++ src/sampleData.js
@@ -0,0 +1,47 @@
+export const userData = [
+  {
+    id: 1,
+    username: 'lifter1',
+    password: 'workout',
+    savedWorkouts: [
+      {
+        target: 'abs',
+        exercise: 'situps'
+      },
+      {
+        target: 'biceps',
+        exercise: 'curls'
+      }
+    ]
+  },
+  {
+    id: 2,
+    username: 'lifter2',
+    password: 'workout',
+    savedWorkouts: [
+      {
+        target: 'abs',
+        exercise: 'situps'
+      },
+      {
+        target: 'biceps',
+        exercise: 'curls'
+      }
+    ]
+  },
+  {
+    id: 3,
+    username: 'lifter3',
+    password: 'workout',
+    savedWorkouts: [
+      {
+        target: 'abs',
+        exercise: 'situps'
+      },
+      {
+        target: 'biceps',
+        exercise: 'curls'
+      }
+    ]
+  }
+]
