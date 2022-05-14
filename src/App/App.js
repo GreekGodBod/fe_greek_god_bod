709b903d0dc158a9e5c8488e36ce4cc1a2d4a74f
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-14 13:19:45 -0600

    add router

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import { newExercises } from '../apiCalls'
 import Login from '../Login/Login'
 import { useState } from 'react'
+import Dashboard from '../Dashboard/Dashboard'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [currentUser, setCurrentUser] = useState([])
@@ -12,6 +14,11 @@ function App() {
         <Login setCurrentUser={setCurrentUser} />
       </section>
       <h1 className='title'>GREEK GOD BOD</h1>
+      <div className='components'>
+        <Routes>
+          <Route exact path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </div>
       {/* <Dashboard currentUser={currentUser} /> */}
     </div>
   )
