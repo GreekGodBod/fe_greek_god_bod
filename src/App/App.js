c406e0e932a40c74c02a40162d010017044350cc
danielsolomon332 <daniel.solomon332@gmail.com>
2022-05-18 18:55:11 -0600

    Import CreateWorkoutForm and set up route

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import { newExercises } from '../apiCalls'
 import Login from '../Login/Login'
 import { useState } from 'react'
 import Dashboard from '../Dashboard/Dashboard'
+import CreateWorkoutForm from '../CreateWorkoutForm/CreateWorkoutForm'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -17,6 +18,10 @@ function App() {
       <div className='components'>
         <Routes>
           <Route exact path='/dashboard' element={<Dashboard />} />
+          <Route exact path='/createworkout' element={<CreateWorkoutForm />} />
+          {/*<Route exact path='/suggestedworkouts' element={<SuggestedWorkouts />} />
+          <Route exact path='/pastworkouts' element={<PastWorkouts />} />
+          <Route exact path='/social' element={<Social />} />*/}
         </Routes>
       </div>
       {/* <Dashboard currentUser={currentUser} /> */}
