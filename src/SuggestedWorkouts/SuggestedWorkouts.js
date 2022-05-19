5ce5580b691428ad0151f00f23f5f10958cd25cf
danielsolomon332 <daniel.solomon332@gmail.com>
2022-05-18 19:33:16 -0600

    Build SuggestedWorkouts component

---
+++ src/SuggestedWorkouts/SuggestedWorkouts.js
@@ -0,0 +1,14 @@
+import './SuggestedWorkouts.css'
+import { useNavigate } from 'react-router-dom'
+
+const SuggestedWorkouts = () => {
+  return (
+    <div className='suggested-workouts-page'>
+      <div className='suggested-workouts-container'>
+        <h1 className='page-title'>Suggested Workouts</h1>
+      </div>
+    </div>
+  )
+}
+
+export default SuggestedWorkouts
