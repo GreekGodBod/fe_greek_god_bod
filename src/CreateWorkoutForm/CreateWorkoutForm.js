7f74cafdebd1547a61177c6ab6407ac2bdfac6dc
danielsolomon332 <daniel.solomon332@gmail.com>
2022-05-18 18:55:48 -0600

    Initial component build for CreateWorkoutForm

---
+++ src/CreateWorkoutForm/CreateWorkoutForm.js
@@ -0,0 +1,14 @@
+import './CreateWorkoutForm.css'
+import { useNavigate } from 'react-router-dom'
+
+const CreateWorkoutForm = () => {
+  return (
+    <div className='create-workout-form'>
+      <div className='form-container'>
+        <h1 className='page-title'>Create Your Own Workout</h1>
+      </div>
+    </div>
+  )
+}
+
+export default CreateWorkoutForm
