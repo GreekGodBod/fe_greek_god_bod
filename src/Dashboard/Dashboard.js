91beb021ed607a09943c97a9d31abb2c94fbce9f
danielsolomon332 <daniel.solomon332@gmail.com>
2022-05-18 18:56:11 -0600

    Import navigation and routes and add onClick event

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,14 +1,21 @@
 import './Dashboard.css'
 import profPic from '../images/lifter-pic.jpg'
+import { useNavigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('/createworkout')
+  }
+
   return (
     <section className='dashboard'>
       <div className='welcome-buttons-container'>
         <h1 className='welcome-message'>Welcome, User!</h1>
         <section className='button-container '>
           <button className='social-button dash-button'>Social</button>
-          <button className='create-workout-button dash-button'>
+          <button className='create-workout-button dash-button' onClick={handleClick}>
             Create Workout
           </button>
           <button className='suggested-button dash-button'>
@@ -22,6 +29,7 @@ const Dashboard = () => {
       <div className='img-container'>
         <img src={profPic} className='profile-pic' alt='user-prof-pic' />
       </div>
+
     </section>
   )
 }
