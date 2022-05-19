2a6ec8ac066465bc20a5852c6fc5dda00453eca6
danielsolomon332 <daniel.solomon332@gmail.com>
2022-05-18 19:33:03 -0600

    Build Social Component

---
+++ src/Social/Social.js
@@ -0,0 +1,14 @@
+import './Social.css'
+import { useNavigate } from 'react-router-dom'
+
+const Social = () => {
+  return (
+    <div className='social-page'>
+      <div className='social-container'>
+        <h1 className='page-title'>Social</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Social
