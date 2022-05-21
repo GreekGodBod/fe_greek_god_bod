9d881d174016a7da1177038d50edb98bdd18d6e4
Geena Jackson <gjacks303@gmail.com>
2022-05-21 15:30:57 -0600

    Add in past workouts

--- src/PastWorkouts/PastWorkouts.js
+++ src/PastWorkouts/PastWorkouts.js
@@ -8,12 +8,13 @@ const PastWorkouts = ({createdWorkouts}) => {
     workouts = createdWorkouts.map(workout => {
         return <Workout key={workout.name} workout={workout}/>
     })
-}
+  }
 
   return (
     <div className='past-workouts-page'>
       <div className='past-workouts-container'>
         <h1 className='page-title'>Past Workouts</h1>
+      {workouts}
       </div>
     </div>
   )
