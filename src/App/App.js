d1c2636b7ba0df105e28d405ab44332266be4b7c
Jake Dunafon <jlad795@gmail.com>
2022-05-21 11:55:50 -0700

    Add setUser

--- src/App/App.js
+++ src/App/App.js
@@ -10,8 +10,8 @@ import SuggestedWorkouts from '../SuggestedWorkouts/SuggestedWorkouts'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  const user = 1
-  const [currentUser, setCurrentUser] = useState([])
+  
+  const [currentUser, setCurrentUser] = useState(null)
   const [pastworkouts, setPastWorkouts] = useState([])
   const [suggestedWorkouts, setSuggestedWorkouts]= useState([])
   const [allExercises, setAllExercises] = useState([])
@@ -20,29 +20,34 @@ function App() {
   useEffect(() => {
     newExercises()
     .then(data => setAllExercises(data))
-    getPastWorkouts(user)
-    .then(data => console.log("PastWorkouts",data))
+   
     // .then(console.log(pastworkouts))
   }, []) 
 
  const addWorkout = (newWorkout) => {
     setCreatedWorkouts([...createdWorkouts, newWorkout]) 
-     
   }
-   
+
+  const setUser = (userId) => {
+    setCurrentUser(userId)
+    getPastWorkouts(userId)
+    .then(data => console.log("PastWorkouts",data))
+  }
+   console.log(currentUser)
   return (
     <div className='App'>
       <section className='login-section'>
-        <Login setCurrentUser={setCurrentUser} />
+      {/* <Login setUser={setUser} /> */}
       </section>
       <h1 className='title'>GREEK GOD BOD</h1>
       <div className='components'>
         <Routes>
-          <Route exact path='/dashboard' element={<Dashboard />} />
-          <Route exact path='/createworkout' element={<CreateWorkoutForm allExercises={allExercises} addWorkout={addWorkout}/>} />
-          <Route exact path='/suggestedworkouts' element={<SuggestedWorkouts />} />
-          <Route exact path='/pastworkouts' element={<PastWorkouts />} />
-          <Route exact path='/social' element={<Social />} />
+          <Route exact path='/login' element={<Login setUser={setUser} />} />
+          <Route exact path='/dashboard/user/:id'  element={<Dashboard currentUser={currentUser}/>} />
+          <Route exact path='/createworkout/user/:id' element={<CreateWorkoutForm allExercises={allExercises} addWorkout={addWorkout}/>} />
+          <Route exact path='/suggestedworkouts/user/:id' element={<SuggestedWorkouts />} />
+          <Route exact path='/pastworkouts/user/:id' element={<PastWorkouts />} />
+          <Route exact path='/social/user/:id' element={<Social />} />
         </Routes>
       </div>
       {/* <Dashboard currentUser={currentUser} /> */}
--- src/sampleData.js
+++ src/sampleData.js
@@ -45,3 +45,60 @@ export const userData = [
     ]
   }
 ]
+
+const completedWorkoutSampleOject = {
+  id: 1,
+  name: 'Chest Day',
+  exercises: [
+    {
+      bodyPart: "chest",
+      equipment: "assisted",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1716.gif",
+      id: "1716",
+      name: "assisted seated pectoralis major stretch with stability ball",
+      target: "pectorals",
+      intervals: [
+        {
+          id: 1,
+          reps: 12,
+          weight_lbs: 130
+        },
+        {
+          id: 2,
+          reps: 12,
+          weight_lbs: 150
+        },
+        {
+          id: 3,
+          reps: 12,
+          weight_lbs: 170
+        }
+      ]
+    },
+    {
+      bodyPart: "chest",
+      equipment: "leverage-machine",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1716.gif",
+      id: "0009",
+      name: "assisted chest dip (kneeling)",
+      target: "pectorals",
+      intervals: [
+        {
+          id: 1,
+          reps: 12,
+          weight_lbs: 130
+        },
+        {
+          id: 2,
+          reps: 12,
+          weight_lbs: 150
+        },
+        {
+          id: 3,
+          reps: 12,
+          weight_lbs: 170
+        }
+      ]
+    }
+  ]
+}
