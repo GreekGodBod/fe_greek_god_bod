d4419285501fc786cf65228adf4c90fc79cde73c
Geena Jackson <gjacks303@gmail.com>
2022-05-21 13:31:48 -0600

    Add redirect for login

--- src/App/App.js
+++ src/App/App.js
@@ -7,7 +7,7 @@ import CreateWorkoutForm from '../CreateWorkoutForm/CreateWorkoutForm'
 import PastWorkouts from '../PastWorkouts/PastWorkouts'
 import Social from '../Social/Social'
 import SuggestedWorkouts from '../SuggestedWorkouts/SuggestedWorkouts'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Redirect, Switch } from 'react-router-dom'
 
 function App() {
   
@@ -41,14 +41,15 @@ function App() {
       </section>
       <h1 className='title'>GREEK GOD BOD</h1>
       <div className='components'>
-        <Routes>
-          <Route exact path='/login' element={<Login setUser={setUser} />} />
-          <Route exact path='/dashboard/user/:id'  element={<Dashboard currentUser={currentUser}/>} />
-          <Route exact path='/createworkout/user/:id' element={<CreateWorkoutForm allExercises={allExercises} addWorkout={addWorkout}/>} />
-          <Route exact path='/suggestedworkouts/user/:id' element={<SuggestedWorkouts />} />
-          <Route exact path='/pastworkouts/user/:id' element={<PastWorkouts />} />
-          <Route exact path='/social/user/:id' element={<Social />} />
-        </Routes>
+          <Routes>
+           <Route path="/" element={<Login replace to="/login" setUser={setUser}/>} />
+            {/* <Route exact path='/login' element={<Login setUser={setUser} />} /> */}
+            <Route exact path='/dashboard/user/:id'  element={<Dashboard currentUser={currentUser}/>} />
+            <Route exact path='/createworkout/user/:id' element={<CreateWorkoutForm allExercises={allExercises} addWorkout={addWorkout}/>} />
+            <Route exact path='/suggestedworkouts/user/:id' element={<SuggestedWorkouts />} />
+            <Route exact path='/pastworkouts/user/:id' element={<PastWorkouts />} />
+            <Route exact path='/social/user/:id' element={<Social />} />
+          </Routes>
       </div>
       {/* <Dashboard currentUser={currentUser} /> */}
     </div>
