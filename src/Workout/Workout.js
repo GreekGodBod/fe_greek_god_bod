9feea84477de60fd03b96278447d6552007bd523
Jake Dunafon <jlad795@gmail.com>
2022-05-21 16:58:05 -0700

    Add findworkout

--- src/Workout/Workout.js
+++ src/Workout/Workout.js
@@ -1,33 +1,40 @@
 import Exercise from '../Exercise/Exercise'
 import './Workout.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Workout = ({ workout, deleteWorkout }) => {
-  const navigate = useNavigate()
-  let exercises
+    let { name } = useParams()
 
-  if (workout) {
-    exercises = workout.exercises.map((exercise) => {
-      return <Exercise key={exercise.id} id={workout.id} exercise={exercise} />
-    })
-  }
+    const navigate = useNavigate()
+    let exercises
 
-  const startWorkout = (workoutName) => {
-    navigate(`/doworkout/${workoutName}`)
-  }
+    if (workout) {
+        exercises = workout.exercises.map((exercise) => {
+            return <Exercise key={exercise.id} id={workout.id} exercise={exercise} />
+        })
+    }
 
-  return (
-    <section className='workout'>
-      <h1>{workout.name}</h1>
-      {exercises}
-      <button
-        className='start-workout-button'
-        onClick={() => startWorkout(workout.name)}
-      >
-        Start Workout
-      </button>
-    </section>
-  )
+    // const findWorkout = (workoutName) => {
+    //     const workout = createdWorkouts.find(workout => workout.name === workoutName)
+    //     return workout
+    //   }
+    const startWorkout = (workoutName) => {
+        // findWorkout(name)
+        navigate(`/doworkout/${workoutName}`)
+    }
+
+    return (
+        <section className='workout'>
+            <h1>{workout.name}</h1>
+            {exercises}
+            <button
+                className='start-workout-button'
+                onClick={() => startWorkout(workout.name)}
+            >
+                Start Workout
+            </button>
+        </section>
+    )
 }
 
 export default Workout
