ac9e2305a704ea64ee19562432cf287e59ade15d
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-21 16:06:31 -0600

    Add startWorkout method

--- src/Workout/Workout.js
+++ src/Workout/Workout.js
@@ -1,22 +1,33 @@
-import Exercise from "../Exercise/Exercise";
+import Exercise from '../Exercise/Exercise'
 import './Workout.css'
+import { useNavigate } from 'react-router-dom'
 
 const Workout = ({ workout, deleteWorkout }) => {
-    let exercises;
+  const navigate = useNavigate()
+  let exercises
 
-    if (workout) {
-        exercises = workout.exercises.map(exercise => {
-            return <Exercise key={exercise.id} id={workout.id} exercise={exercise} />
-        })
-    }
+  if (workout) {
+    exercises = workout.exercises.map((exercise) => {
+      return <Exercise key={exercise.id} id={workout.id} exercise={exercise} />
+    })
+  }
 
-    return (
-        <section className="workout">
-            <h1>{workout.name}</h1>
-            {exercises}
-            <button className='start-workout-button'>Start Workout</button>
-        </section>
-    )
+  const startWorkout = (workoutName) => {
+    navigate(`/doworkout/${workoutName}`)
+  }
+
+  return (
+    <section className='workout'>
+      <h1>{workout.name}</h1>
+      {exercises}
+      <button
+        className='start-workout-button'
+        onClick={() => startWorkout(workout.name)}
+      >
+        Start Workout
+      </button>
+    </section>
+  )
 }
 
 export default Workout
