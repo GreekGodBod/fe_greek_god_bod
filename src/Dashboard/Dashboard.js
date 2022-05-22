7c4fd9424c0d5a1786f2c711716aec852ffd9ac3
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-22 13:25:21 -0600

    update format

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -2,9 +2,8 @@ import './Dashboard.css'
 import profPic from '../images/lifter-pic.jpg'
 import { useNavigate } from 'react-router-dom'
 
-
-const Dashboard = ({currentUser}) => {
-  console.log("dash",currentUser)
+const Dashboard = ({ currentUser }) => {
+  console.log('dash', currentUser)
   const navigate = useNavigate()
   const navigateCreateWorkout = () => {
     navigate(`/createworkout/user/${currentUser}`)
@@ -27,14 +26,28 @@ const Dashboard = ({currentUser}) => {
       <div className='welcome-buttons-container'>
         <h1 className='welcome-message'>Welcome, User!</h1>
         <section className='button-container '>
-          <button className='social-button dash-button' onClick={navigateSocial}>Social</button>
-          <button className='create-workout-button dash-button' onClick={navigateCreateWorkout}>
+          <button
+            className='social-button dash-button'
+            onClick={navigateSocial}
+          >
+            Social
+          </button>
+          <button
+            className='create-workout-button dash-button'
+            onClick={navigateCreateWorkout}
+          >
             Create Workout
           </button>
-          <button className='suggested-button dash-button' onClick={navigateSuggestedWorkouts}>
+          <button
+            className='suggested-button dash-button'
+            onClick={navigateSuggestedWorkouts}
+          >
             Suggested Workouts
           </button>
-          <button className='past-workout-button dash-button' onClick={navigatePastWorkouts}>
+          <button
+            className='past-workout-button dash-button'
+            onClick={navigatePastWorkouts}
+          >
             Past Workouts
           </button>
         </section>
@@ -42,7 +55,6 @@ const Dashboard = ({currentUser}) => {
       <div className='img-container'>
         <img src={profPic} className='profile-pic' alt='user-prof-pic' />
       </div>
-
     </section>
   )
 }
