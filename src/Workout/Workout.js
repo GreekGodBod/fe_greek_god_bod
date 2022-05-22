1cd1e7d9a5aa4e735fb8d80b552319cee88a520f
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-22 13:48:43 -0600

    Add classname to workout name

--- src/Workout/Workout.js
+++ src/Workout/Workout.js
@@ -3,24 +3,32 @@ import './Workout.css'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const Workout = ({ workout, deleteWorkout, currentUser, findWorkout }) => {
-    let { name } = useParams()
+  let { name } = useParams()
 
-    const navigate = useNavigate()
-    let exercises
+  const navigate = useNavigate()
+  let exercises
 
-    if (workout) {
-        exercises = workout.exercises.map((exercise) => {
-            return <Exercise key={exercise.id} workoutName={workout.name} currentUser={currentUser} id={workout.id} exercise={exercise} />
-        })
-    }
-    const startWorkout = (workoutName) => {
-        findWorkout(workoutName)
-        navigate(`/doworkout/${workoutName}/user/${currentUser}`)
-    }
+  if (workout) {
+    exercises = workout.exercises.map((exercise) => {
+      return (
+        <Exercise
+          key={exercise.id}
+          workoutName={workout.name}
+          currentUser={currentUser}
+          id={workout.id}
+          exercise={exercise}
+        />
+      )
+    })
+  }
+  const startWorkout = (workoutName) => {
+    findWorkout(workoutName)
+    navigate(`/doworkout/${workoutName}/user/${currentUser}`)
+  }
 
   return (
     <section className='workout'>
-      <h1>{workout.name}</h1>
+      <h1 className='workout-name'>{workout.name}</h1>
       {/* {exercises} commented out because may be better to not show exercises
       since we will have long list of workouts, potentially */}
       <button
