c87dd604ea8665c7e40d60a9e019619f397157b6
Jake Dunafon <jlad795@gmail.com>
2022-05-23 17:49:27 -0700

    Add state for all sets

--- src/DoWorkout/DoWorkout.js
+++ src/DoWorkout/DoWorkout.js
@@ -4,47 +4,50 @@ import { useState } from 'react'
 import './DoWorkout.css'
 
 const DoWorkout = (props) => {
-const [sets, setSets] = useState('')
-  const navigate = useNavigate()
+    const [allSets, setAllSets] = useState([])
+    const navigate = useNavigate()
+    console.log(allSets)
+    
 
-  const addSets = (newSet) => {
-    setSets([...sets, newSet])
-  }
+    //find the right exercise with .find? and then
+    //excerise.intervals.push(newSet) with reps and weight_lbs
+    //send the oneWorkout with the intervals in the patch 
+    
 
 
-  let exercises = props.oneWorkout.exercises.map((exercise) => {
+    let exercises = props.oneWorkout.exercises.map((exercise) => {
+        return (
+            <Exercise
+                key={exercise.id}
+                exercise={exercise}
+                id={props.oneWorkout.id}
+                setAllSets={setAllSets}
+                allSets={allSets}
+            />
+        )
+    })
+
+    const navigateMyWorkouts = () => {
+        navigate(`/myworkouts/user/${props.currentUser}`)
+    }
+
     return (
-      <Exercise
-        key={exercise.id}
-        exercise={exercise}
-        id={props.oneWorkout.id}
-        addSets={addSets}
-        sets={sets}
-      />
-    )
-  })
-
-  const navigateMyWorkouts = () => {
-    navigate(`/myworkouts/user/${props.currentUser}`)
-  }
-
-  return (
-    <div className='do-workout-page'>
-      <section className='header-create-workout'>
-        <div className='back-to-dashboard'>
-          <button
-            className='back-to-dashboard-button'
-            onClick={navigateMyWorkouts}
-          >
-            Back to My Workouts
-          </button>
+        <div className='do-workout-page'>
+            <section className='header-create-workout'>
+                <div className='back-to-dashboard'>
+                    <button
+                        className='back-to-dashboard-button'
+                        onClick={navigateMyWorkouts}
+                    >
+                        Back to My Workouts
+                    </button>
+                </div>
+                <h1 className='page-title'>{props.oneWorkout.name}</h1>
+                <div className='spacer'></div>
+            </section>
+            <section className='do-workout-container'>{exercises}</section>
         </div>
-        <h1 className='page-title'>{props.oneWorkout.name}</h1>
-        <div className='spacer'></div>
-      </section>
-      <section className='do-workout-container'>{exercises}</section>
-    </div>
-  )
+    )
 }
 
 export default DoWorkout
