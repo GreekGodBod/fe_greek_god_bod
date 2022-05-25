aae5055a41d862fa60a1f159daa500ec4042d6bd
Geena Jackson <gjacks303@gmail.com>
2022-05-25 12:23:33 -0600

    Add fixture to dash

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,9 +1,25 @@
 describe('Dashboard page load', () => {
   beforeEach(() => {
+    cy.intercept('GET', 'https://exercisedb.p.rapidapi.com/exercises', {
+      statusCode: 200,
+      fixture: 'exercises.json'
+      })
+
       cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/user', {
         statusCode: 200,
         fixture: 'users.json'
-        }).as('users')
+        })
+
+      cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/user/1', {
+        statusCode: 200,
+        fixture: 'pastWorkouts.json'
+        })
+
+      cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/workouts', {
+        statusCode: 200,
+        fixture: 'suggestedWorkout.json'
+      })
+
       cy.visit('http://localhost:3000/dashboard/user/1');
       })
 
