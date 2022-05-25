bf373ebfbf532c8b11a43213ae2c70aa43a80844
Geena Jackson <gjacks303@gmail.com>
2022-05-24 19:48:21 -0600

    Create intial tests

---
+++ cypress/integration/suggestedworkouts_spec.js
@@ -0,0 +1,19 @@
+describe('Login page load', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/workouts', {
+      statusCode: 200,
+      fixture: 'suggestedWorkout.json'
+      })
+    cy.visit('http://localhost:3000/suggestedworkouts/user/1');
+    })
+    
+    it('should display a header', () => {
+    cy.get('.page-title')
+     .should('have.text', 'Suggested Workouts')
+    })
+
+    it('should display a back to dashboard button', () => {
+      cy.get('.back-to-dashboard-button')
+       .should('have.text', 'Back to Dashboard')
+      })
+  })
