da2795a613f149fafffb2c02696b683f32191ed8
Geena Jackson <gjacks303@gmail.com>
2022-05-25 12:21:07 -0600

    Add fixtures to create workout

---
+++ cypress/integration/createworkout_spec.js
@@ -0,0 +1,33 @@
+describe('Create Workout page load', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://exercisedb.p.rapidapi.com/exercises', {
+      statusCode: 200,
+      fixture: 'exercises.json'
+      })
+
+      cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/user', {
+        statusCode: 200,
+        fixture: 'users.json'
+        })
+
+      cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/user/1', {
+        statusCode: 200,
+        fixture: 'pastWorkouts.json'
+        })
+
+      cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/workouts', {
+        statusCode: 200,
+        fixture: 'suggestedWorkout.json'
+      })
+
+    cy.visit('http://localhost:3000/createworkout/user/1');
+    })
+
+    it('should display a header called Create Your Workout', () => {
+      cy.get('.page-title')
+       .should('have.text', 'Create Your Workout')
+      })
+
+
+
+})
