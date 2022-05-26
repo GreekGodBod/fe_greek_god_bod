4a14b028aabae035911cd2ea97dd0c5a95afffba
Geena Jackson <gjacks303@gmail.com>
2022-05-25 19:46:34 -0600

    Create initial tests for social

---
+++ cypress/integration/social_spec.js
@@ -0,0 +1,54 @@
+describe('Social page load', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://exercisedb.p.rapidapi.com/exercises', {
+      statusCode: 200,
+      fixture: 'exercises.json'
+      })
+
+    cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/user', {
+      statusCode: 200,
+      fixture: 'users.json'
+      })
+
+    cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/user/1', {
+      statusCode: 200,
+      fixture: 'pastWorkouts.json'
+      })
+
+      cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/user/null', {
+      statusCode: 200,
+      fixture: 'pastWorkouts.json'
+      })
+
+    cy.intercept('GET', 'https://be-greek-god-bod.herokuapp.com//api/v1/workouts', {
+      statusCode: 200,
+      fixture: 'suggestedWorkout.json'
+    })
+
+
+    cy.visit('http://localhost:3000/');
+    })
+
+    it('should be able to log in', () => {
+      cy.get('.username-input')
+      .type('user_1')
+      .get('.password-input')
+      .type('password1')
+      .get('.login-button')
+      .click()
+    })
+
+    it('should be able to click on Social', () => {
+      cy.get('.username-input')
+      .type('user_1')
+      .get('.password-input')
+      .type('password1')
+      .get('.login-button')
+      .click()
+      cy.get('.social-button')
+      .click()
+    })
+
+   
+    
+  })
