557b6f365950d3962d8a2a2753e8b4f5829ab0a8
Jake Dunafon <jlad795@gmail.com>
2022-05-25 17:56:29 -0700

    Add .message to data

--- src/ChatBox/ChatBox.js
+++ src/ChatBox/ChatBox.js
@@ -42,7 +42,7 @@ const ChatBox = (props) => {
     const handlers = {
       received(data) {
         console.log("data",data)
-        setMessages([...messages, data])
+        setMessages([...messages, data.message])
       },
 
       connected() {
